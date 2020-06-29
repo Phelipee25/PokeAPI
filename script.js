222f649d917e355b360057378293c5789ce03db3
Phelipe Mazzolli <phelipe.mazzolli@gmail.com>
2020-06-29 18:36:49 -0300

    subindo alteração da api

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 async function confirmaPKM(){
     let pkm = document.getElementById("busca").value.toLowerCase()
-    // console.log(pkm)
     axios.get(`https://pokeapi.co/api/v2/pokemon/${pkm}`)
     .then ((resposta)=>{
         let pokemon = resposta.data
         let nome = pokemon.name
         let id= pokemon.id.toString()
+        let habilidades = []
+        pokemon.abilities.forEach((habilidade) => {
+            habilidades.push(habilidade.ability.name)
+        })
+        let tipos = []
+        pokemon.types.forEach((tipo) => {
+            tipos.push(tipo.type.name)
+        })
         mostraNome(nome)
         mostraImg(id)
+        mostraTipo(tipos)
+        mostraHabilidade(habilidades[0])
+        trocaTipo(habilidades[0+1])
     })
 }
+function mostraTipo(tipoDoPKM){
+    let tipoPkm = document.getElementById("type")
+    tipoPkm.innerText = tipoDoPKM
+    
+}
+function mostraHabilidade(habilidadeDoPKM){
+    let habilidadePkm = document.getElementById("ability")
+    habilidadePkm.innerText = habilidadeDoPKM
+}
+function trocaTipo(trocaHabiPKM){
+    let trocaHabilidadePkm = document.getElementById("ability")
+    trocaHabilidadePkm.innerText = trocaHabiPKM
+}
 function mostraNome(nomeDoPoke){
-    let nomePkm = document.getElementById("NomePKM")
+    let nomePkm = document.getElementById("nomePKM")
     nomePkm.innerHTML = nomeDoPoke
 }
 function mostraImg(id, imgDoPoke){
@@ -26,6 +49,6 @@ function mostraImg(id, imgDoPoke){
 }
     $("#busca").keyup(function (event) {
     if (event.keyCode === 13) {
-      $("#botao").click();
+      $("#confirma").click();
     }
     });
